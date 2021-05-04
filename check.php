308f1f6ae7a7bb65a1552caa91347cbc355b20d8
Arya Purwadana <purwadanaarya@gmail.com>
2021-05-04 15:01:12 +0800

    check.php

---
+++ check.php
@@ -0,0 +1,16 @@
+<?php 
+	$ip = $_GET['ip'];
+// 	$databaseHost = 'localhost';
+// 	$databaseName = 'licenses';
+// 	$databaseUsername = 'root';
+// 	$databasePassword = '';
+// 	$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+// 	$result = mysqli_query($mysqli, "SELECT * FROM license_ip WHERE ip='".$ip."'");
+// 	$ip_data = mysqli_fetch_array($result);
+// 	if(mysqli_num_rows($result)>0){
+// 		$data = 1;
+// 	}else{
+// 		$data = 0;
+// 	}
+// 	echo json_encode($data);
+?>
